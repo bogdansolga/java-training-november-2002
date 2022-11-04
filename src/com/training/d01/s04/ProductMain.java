d5beaac377fb178d5d63e880800107d33441d108
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-04 12:08:16 +0200

    [dev] Made the Product class immutable

--- src/com/training/d01/s04/ProductMain.java
+++ src/com/training/d01/s04/ProductMain.java
@@ -9,13 +9,20 @@ public class ProductMain {
 
     public static void main(String[] args) {
         Product telefon = createProduct(20, "Telefon");
-        System.out.println("The first product is a " + telefon.getName());
+        //System.out.println("The first product is a " + telefon.getName());
 
         Product înghețată = createProduct(10, "Înghețată");
-        System.out.println("The second product is an '" + înghețată.getName() + "'");
+        //System.out.println("The second product is an '" + înghețată.getName() + "'");
 
         // an array of products
         Product[] products = {telefon, înghețată};  // fixed size
+        Product[] another = new Product[5]; // an array of length 5, without any items in it
+        another[3] = telefon;
+
+        // for (data-type variable-name : iterated-collection-or-array)
+        for (Product product : another) {
+            System.out.println(product);
+        }
 
         // a collection of products
         List<Product> productList = new ArrayList<>(1); // dynamic size
@@ -24,12 +31,15 @@ public class ProductMain {
 
         Product x = null; // a variable of type Product, not initialized (there is no 'new Product()')
         System.out.println(x.getName());
+
+        // invoking a static method
+        Product staticallyCreated = Product.createProduct(20, "Flight ticket");
     }
 
     private static Product createProduct(int id, String name) { // Shift + F6 --> Rename
-        Product theProduct = new Product();
-        theProduct.setId(id);
-        theProduct.setName(name);
+        Product theProduct = new Product(id, name);
+        //theProduct.setId(id); --> cannot be set anymore
+        //theProduct.setName(name);
         return theProduct;
     }
 }
--- src/com/training/model/Product.java
+++ src/com/training/model/Product.java
@@ -1,23 +1,39 @@
 package com.training.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
     // properties / fields
-    private int id;
-    private String name;
+
+    private static final String TABLE_NAME = "Product";
+
+    private static final List<String> STRINGS; // blank final variable
+
+    static {
+        System.out.println("Initializing the class...");
+
+        STRINGS = new ArrayList<>(10);
+        STRINGS.add("something");
+    }
+
+    private final int id;
+    private final String name;
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static Product createProduct(int id, String name) {
+        return new Product(id, name);
     }
 }
