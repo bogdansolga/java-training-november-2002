1a8779b476847ff0f7b41508c01de732837edc29
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-15 14:13:18 +0200

    [dev] Added the fruits samples draft class

---
+++ src/com/training/d04/s04/FruitsCollectionsMain.java
@@ -0,0 +1,28 @@
+package com.training.d04.s04;
+
+public class FruitsCollectionsMain {
+
+    public static void main(String[] args) {
+        listsOfFruits();
+
+        setsOfFruits();
+
+        mapsOfFruits();
+    }
+
+    private static void listsOfFruits() {
+        // use a List of Apples or Bananas
+        // use a List of AbstractFruit
+
+        // add elements, display them, remove them
+    }
+
+    private static void setsOfFruits() {
+        // use a HashSet, a TreeSet and a LinkedHashSet
+    }
+
+    private static void mapsOfFruits() {
+        // use a HashMap and a TreeMap
+        // use the fruits as values, the keys can be anything (Integer, String, ...)
+    }
+}
