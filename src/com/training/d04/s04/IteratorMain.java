a558dde3ed2cacd1467f582a398c715f4c8b3c94
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-16 09:44:44 +0200

    [dev] Added the iterator exercise and the TODO for it

--- src/com/training/d04/s04/IteratorMain.java
+++ src/com/training/d04/s04/IteratorMain.java
@@ -8,6 +8,19 @@ import java.util.List;
 public class IteratorMain {
 
     public static void main(String[] args) {
+        stringIteratorExample();
+
+        fruitsIteratorExample();
+    }
+
+    private static void fruitsIteratorExample() {
+        // TODO
+        //  create a collection of fruits (List or Set, as you wish)
+        //  add some fruits to it
+        //  iterate over the collection, remove the fruits from which we cannot make juice, display the others
+    }
+
+    private static void stringIteratorExample() {
         List<String> strings = new ArrayList<>(
                 Arrays.asList("Java is a nice programming language, pretty elegant and almost simple".split(" ")));
         System.out.println(strings.size());
